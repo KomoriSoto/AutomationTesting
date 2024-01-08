13718bd49728fcbdcf8b14c1918ec2f2f9709cac
LAPTOP-MCS6V31M\user <aiunaaiuna71@gmail.com>
2024-01-08 12:25:23 +0900

    first commit

---
+++ src/test/ExecTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static com.codeborne.selenide.Condition.*;
+
+import org.junit.Test;
+
+import com.codeborne.selenide.Selenide;
+
+
+public class ExecTest {
+	@Test
+	public void executeTest() {
+		// test.jspにアクセス
+	    Selenide.open("http://localhost:8080/AutomationTesting/test/test.jsp");
+	    // Hello Worldという内容が書かれているかチェック
+	    Selenide.$(".test").shouldHave(text("Hello World"));
+	}
+}
